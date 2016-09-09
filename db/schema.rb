94c46d3559b2baf46266797f7d93f6ff990e351c
Mattmcdevitt93 <Mattmcdevitt93@gmail.com>
2016-09-08 20:00:47 -0400

    gem file fix

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20160831135655) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "standing"
+    t.string   "name",       null: false
+    t.integer  "standing",   null: false
     t.datetime "expire?"
     t.string   "notes"
     t.datetime "created_at", null: false
@@ -66,7 +69,7 @@ ActiveRecord::Schema.define(version: 20160831135655) do
     t.datetime "character_cake_day"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
