b6ee1b725e8cd3dc3a7e68c5d3803d1f27d68a69
Mattmcdevitt93 <Mattmcdevitt93@gmail.com>
2017-03-30 22:08:03 -0400

    disabled discord test

--- config/initializers/discordrb.rb
+++ config/initializers/discordrb.rb
@@ -6,8 +6,8 @@ $discord_server_id = 'Mjk3MTA5MzQ2NTY2MDEyOTI4.C78w2g.5dg9TNdYD8VyMGYqBm4YvHMsXK
 
 bot = Discordrb::Bot.new token: $discord_server_id, client_id: 297109346566012928
 
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
-end
+# bot.message(with_text: 'Ping!') do |event|
+#   event.respond 'Pong!'
+# end
 
-bot.run
+# bot.run
