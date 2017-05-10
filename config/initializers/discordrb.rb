52e016e38a71ab06fe2ed24267df66ce6a821232
Mattmcdevitt93 <Mattmcdevitt93@gmail.com>
2017-05-10 13:31:35 -0400

    updated discord bot with command list and time feature

--- config/initializers/discordrb.rb
+++ config/initializers/discordrb.rb
@@ -1,5 +1,11 @@
 require 'discordrb'
 
+class Time
+  def prime?
+    hour >= 18
+  end
+end
+
 if ENV["DISCORD_SERVER"] != nil && ENV["DISCORD_CLIENT"] != nil && ENV["DISCORD_TOKEN"] != nil
 
 	$bot = Discordrb::Commands::CommandBot.new token: ENV["DISCORD_TOKEN"], client_id: ENV["DISCORD_CLIENT"]
@@ -34,14 +40,52 @@ if ENV["DISCORD_SERVER"] != nil && ENV["DISCORD_CLIENT"] != nil && ENV["DISCORD_
 
 
 	$bot.mention() do |event|
-		event.respond Time.now.to_s
-		# event.respond event.author.name.to_s
+		note = event.message.to_s.downcase.gsub!(/\s+/, '')
+		Rails.logger.info "Discord Mention: " + note.to_s
+		if note.to_s.blank? or note.include? "help" or note.include? "list" or note.include? "commands"
+			event.respond "
 
-		event.respond event.message.to_s
+Bot Command List: 
+
+Help = This is redundant... you just asked for help.
+Time = Tells you the time of all the major time zones (* marks current prime times)
+
+			"
+		else
+
+			if note.include? "time"
+
+				zone = ActiveSupport::TimeZone.new("Etc/UTC")
+				prime = Time.now.in_time_zone(zone).prime? ? '*' : ''
+				event.respond "UTC" + prime + " - " + Time.now.in_time_zone(zone).to_s
+
+				zone = ActiveSupport::TimeZone.new("Eastern Time (US & Canada)")
+				prime = Time.now.in_time_zone(zone).prime? ? '*' : ''
+				event.respond "EST" + prime + " - " + Time.now.in_time_zone(zone).to_s
+
+				zone = ActiveSupport::TimeZone.new("Pacific Time (US & Canada)")
+				prime = Time.now.in_time_zone(zone).prime? ? '*' : ''
+				event.respond "PST" + prime + " - " + Time.now.in_time_zone(zone).to_s
+
+				zone = ActiveSupport::TimeZone.new("Australia/Sydney")
+				prime = Time.now.in_time_zone(zone).prime? ? '*' : ''
+				event.respond "AEST" + prime + " - " + Time.now.in_time_zone(zone).to_s
+
+				zone = ActiveSupport::TimeZone.new("Europe/Moscow")
+				prime = Time.now.in_time_zone(zone).prime? ? '*' : ''
+				event.respond "MSK" + prime + " - " + Time.now.in_time_zone(zone).to_s
+			end
+
+			if note.include? "openthepodbaydoorshal" or note.include? "openthepodbaydoors"
+				event.respond "I'm sorry, Dave."
+				event.respond "I'm afraid I can't do that."
+
+			end
+		end
 	end
 
 	$bot.ready do |event|
-		$bot.game = "Beta version 0.1"
+		$bot.game = "Beta version 0.2"
 		# $bot.send_message(ENV["DISCORD_SERVER"], 'Auth-bot is online!', tts = false, embed = nil)
 	end
 
