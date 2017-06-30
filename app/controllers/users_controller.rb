08c4aa8c878a600cfddf9631ffe4a96f6b21fe34
Mattmcdevitt93 <Mattmcdevitt93@gmail.com>
2017-06-30 14:09:33 -0400

    change sorttable and added clock to new timesheets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
     before_action :valid_check, only: []
     before_action :admin_check, only: [:index]
-
+    before_action :director_check, only: [:stats]
 
 	def index
     @user = User.paginate(:page => params[:page], :per_page => 50).order(id: :desc)
     @blacklist = Blacklist.new
 	end
+
+  def stats
+    @user = User.where('corp_ticker' => current_user.corp_ticker).order(id: :desc)
+
+  end
   
   def show
     @stats = 5
